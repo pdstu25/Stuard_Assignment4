6ba907e72f741e4d1bcaae9445eed18921f15e8e
David <pdstu25@gmail.com>
2022-04-12 21:14:09 -0500

    Added query to file

--- app/src/main/java/com/example/stuard_assignment4/database/NoteDao.kt
+++ app/src/main/java/com/example/stuard_assignment4/database/NoteDao.kt
@@ -1,9 +1,11 @@
 package com.example.stuard_assignment4.database
 
 import androidx.room.Dao
+import androidx.room.Query
 
 @Dao
 interface NoteDao {
 
+    @Query("SELECT * FROM Note")
     fun getAllNotes() : List<Note>
 }
