5f304c2bfbeb671475d5d9a12a5a2aef88a2f77a
David <pdstu25@gmail.com>
2022-04-19 15:49:01 -0500

    getCurrentTime added to database Add/Update

--- app/src/main/java/com/example/stuard_assignment4/NoteActivity.kt
+++ app/src/main/java/com/example/stuard_assignment4/NoteActivity.kt
@@ -70,16 +70,17 @@ class NoteActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             val noteDao = AppDatabase.getDatabase(applicationContext)
                 .noteDao()
+            var currentTime = LastModified()
 
             if (purpose.equals(getString(R.string.intent_purpose_add_note))) {
                 // add the new person to the database
                 // when adding, set primary key (id) to 0
-                val notes = Note(0, noteTitle, userNotes, "9999")
+                val notes = Note(0, noteTitle, userNotes, "${currentTime.getCurrentTime()}")
                 noteId = noteDao.addNote(notes)
                 Log.i("STATUS_NAME", "inserted new note: ${notes}")
             } else {
                 // update a current person in the database
-                val notes = Note(noteId, noteTitle, userNotes, "9999")
+                val notes = Note(noteId, noteTitle, userNotes, "${currentTime.getCurrentTime()}")
                 noteDao.updateNotes(notes)
                 Log.i("STATUS_NAME", "updated existing person: ${notes}")
             }
